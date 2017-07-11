9bd1e5f729ed1ca164aa24061d77f1aa02646f1e
jin xu <pioevh@163.com>
2017-07-11 19:10:33 +0800

    done

--- commonlib/ex.py
+++ commonlib/ex.py
@@ -1,32 +1,8 @@
-# /usr/bin/python3
-# -*- coding: utf-8 -*-
+from ffnet import imlgraph, ffnet
+import networkx as NX
+import pylab
 
-import traceback
-from snack import *
- 
-def window(screen):
-  btn1=Button('B1')  #实例化一个按钮组件
-  btn2=Button('B2')  #实例化一个按钮组件
-  g=Grid(2,1)   #实例化一个两列，一行的网格
-  g.setField(btn1,0,0)  #把组件填充到网格中
-  g.setField(btn2,1,0)
-  screen.gridWrappedWindow(g,'Windows')
-  f=Form()  #实例化一个form
-  f.add(g)    #把网格填充到form
-  result=f.run()
-  screen.popWindow()
-  btn1.setCallback() 
-  btn2.setCallback() 
-
-def main():
-  try:
-    screen=SnackScreen()  #实例化一个snack界面
-    window(screen)
-  except:
-    print(traceback.format_exc())
-  finally:
-    screen.finish()  #关闭snack界面
-    return('')
-
-if(__name__=='__main__'):
-  main()
+conec = imlgraph((3, [(3,), (6, 3), (3,)], 3), biases=False)
+net = ffnet(conec)
+NX.draw_networkx(net.graph, prog='dot')
+pylab.show()
--- commonlib/zipfilelib.py
+++ commonlib/zipfilelib.py
@@ -1,4 +1,4 @@
-#!python
+#!python3
 # coding=utf-8
 '''
   zipfilelib.py
@@ -11,19 +11,29 @@ import time
 
 
 def zipfilelib(testFile): 
+  with zipfile.ZipFile('1.zip', 'w') as zf:
+    zf.write(testFile) 
+    zf.write('ex.py') 
+  time.sleep(1)
+  with zipfile.ZipFile('1.zip', 'r') as zf:
+    print(zf.namelist())
+    print(zf.infolist())
+    print(zf.read(testFile).decode('utf-8'))
+
+def makeFile(testFile):
   content='test zipfile\n\n1234567890\nabcdefghijklmnopqrstuvwxyz'
   f=open(testFile,'w')
   f.write(content)
   f.close
-  print(os.listdir())
-  time.sleep(1)
-   
+  print(testFile in os.listdir())
 
 def clean(testFile):
   os.remove(testFile)
-  print(os.listdir())
+  os.remove('1.zip')
+  print(testFile in os.listdir())
 
 if(__name__=='__main__'):
-  testFile='test.txt'
+  testFile='1.txt'
+  makeFile(testFile)
   zipfilelib(testFile)
   clean(testFile)
