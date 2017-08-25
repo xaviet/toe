4bc19f133764ef32c8a5b8e479cef6906ceeeabf
pv <pioevh@163.com>
2017-08-25 16:34:02 +0800

    done

---
+++ commonlib/xmlrpclib.py
@@ -0,0 +1,10 @@
+#! /usr/bin/python3
+# coding=utf-8
+'''
+  xmlrpclib.py
+'''
+
+'''
+https://docs.python.org/3/library/xmlrpc.client.html#module-xmlrpc.client
+https://docs.python.org/3/library/xmlrpc.server.html#module-xmlrpc.server
+'''
